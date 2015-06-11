975013b0ae74313a1398fa2522d3ee4574b7a83f
Josh Johns <joshcjohns@gmail.com>
2015-06-11 10:44:23 -0400

    Adding older files.

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello world!");
+  puts("Print line 2");
+  puts("Another.");
+  puts("When");
+  puts("4");
+  puts("nee");
+
+  return 0;
+
+
+}
---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 6;
+	int height = 72;
+
+	printf("I am %d years\a old.\n", age);
+	printf("I\t am %d\v inches tall.\n", height);
+
+	return 0;
+}
